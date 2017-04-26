936de5a42967a010b1d23c9427e08aaabf752e3d
IsaacGuan <yanranguan1994@gmail.com>
2017-04-26 08:17:35 +0800

    first commit

---
+++ Assets/ARMonsters/Scripts/QuitGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+    int escapeTimes = 1;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            GUI.Label(new Rect(450, 180, 100, 20), "再按一次退出");
+            escapeTimes++;
+
+            StartCoroutine("resetTimes");
+            if (escapeTimes > 1)
+            {
+                Application.Quit();
+            }
+        }
+	}
+
+    IEnumerator resetTimes()
+    {
+        yield return new WaitForSeconds(1);
+        escapeTimes = 1;
+    }
+
+}
---
+++ Assets/Easy Weapons/Scripts/TimedObjectDestroyer.cs
@@ -0,0 +1,54 @@
+﻿/// <summary>
+/// TimedObjectDestroyer.cs
+/// Author: MutantGopher
+/// This script destroys a GameObject after the number of seconds specified in
+/// the lifeTime variable.  Useful for things like explosions and rockets.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class TimedObjectDestroyer : MonoBehaviour
+{
+	public float lifeTime = 10.0f;
+
+    /*
+    public string objectType;
+    public static int destroyNum = 0;
+
+    private GUIStyle guiStyle = new GUIStyle();
+     * */
+
+
+	// Use this for initialization
+	void Start()
+	{
+		Destroy(gameObject, lifeTime);
+
+        /*
+        if (!GameObject.Find("enemy"))
+        {
+            if (objectType == "enemy")
+            {
+                destroyNum++;
+            }
+        }
+         * */
+
+    }
+
+    /*
+    void OnGUI()
+    {
+        guiStyle.fontSize = 120;
+        guiStyle.normal.textColor = new Color(255f / 256f, 164f / 256f, 0f / 256f, 255f / 256f);
+        if (objectType == "enemy")
+        {
+            GUI.Label(new Rect(320, 300, 100, 20), "×" + destroyNum, guiStyle);
+        }
+
+    }
+     * */
+
+
+}
